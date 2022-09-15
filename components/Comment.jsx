39f0052f6cc691ec2cf1d63ba28cc64cd35c2bbe
sakuraii <pearl.k.sakura@gmail.com>
2022-09-15 14:55:33 +0700

    not finish but out of time

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,13 +1,39 @@
 import React from "react";
+import { comments } from "../libs/comments";
 import Reply from "./Reply";
 
 export default function Comment(props) {
+  const comment = comments;
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      
+        {comment.map((x,i) => ( <div key={i}>
+          <div className="d-flex gap-2 my-2" >
+            <img
+              src={comment[i].userImagePath}
+              width="48"
+              height="48"
+              className="rounded-circle"
+              style={{ objectFit: "cover" }}
+            />
+            <div
+              className="rounded rounded-3 p-2"
+              style={{ backgroundColor: "#3A3B3C" }}
+            >
+              <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+                {comment[i].username}
+              </span>
+              <br />
+              <span style={{ color: "#E4E6EB" }}>{comment[i].commentText}</span>
+              <div className="d-flex align-items-center gap-1">
+                <img src="/like.svg" width={20}></img>
+                <span style={{ color: "#B0B3B8" }}>{comment[i].likeNum}</span>
+              </div>
+            </div>
+          </div>
+        </div>) )}
 
-      {/*render Reply here... */}
     </div>
   );
-}
+        }
--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { comments } from "../libs/comments";
 
 export default function Reply(props) {
-  return <div></div>;
+  const reply = props.reply;
+  return (
+      // <div>
+      //   {reply.map((x,i) => (
+      //     <div className="d-flex gap-2 my-2 ps-5">
+      //     <img
+      //       src={reply.userImagePath}
+      //       width="48"
+      //       height="48"
+      //       className="rounded-circle"
+      //       style={{ objectFit: "cover" }}
+      //     />
+      //     <div
+      //       className="rounded rounded-3 p-2"
+      //       style={{ backgroundColor: "#3A3B3C" }}
+      //     >
+      //       <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+      //         หมาน้อย
+      //       </span>
+      //       <br />
+      //       <span style={{ color: "#E4E6EB" }}>{reply.replytext}</span>
+      //     </div>
+      //   </div>
+      //   ))}
+          
+      //   </div>
+      <div></div>
+    
+  );
 }
